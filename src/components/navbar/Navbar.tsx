07e396bc23e95782a8f382b76a78dc3490170095
vitoria <vitorialima358@gmail.com>
2026-01-15 11:40:46 -0300

    atualiza telas e rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
   return (
     <>
       <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
+
         <div className="container flex justify-between text-lg mx-8">
-          Blog Pessoal
+          <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
           <div className='flex gap-4'>
             Postagens
@@ -19,4 +22,3 @@ function Navbar() {
 }
 
 export default Navbar
-
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,36 +1,38 @@
+import home from "../../assets/home.png";
+
+
 function Home() {
   return (
-    <>
-      <div className="bg-indigo-900 flex justify-center">
-        <div className="container grid grid-cols-2 text-white">
-          <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className="text-5xl font-bold">
-              Seja Bem Vinde!
-            </h2>
-            <p className="text-xl">
-              Expresse aqui seus pensamentos e opiniões
-            </p>
+    <div className="flex justify-center bg-indigo-900 text-white min-h-[80vh]">
+      <div className="container grid grid-cols-2 items-center gap-8 px-4">
 
-            <div className="flex justify-around gap-4">
-              <div
-                className="rounded text-white border-white border-solid border-2 py-2 px-4"
-              >
-                Nova Postagem
-              </div>
-            </div>
-          </div>
+        {/* COLUNA ESQUERDA – TEXTO */}
+        <div className="flex flex-col gap-4 mt-16">
+          <h1 className="text-5xl font-bold">
+            Seja Bem Vinde!
+          </h1>
 
-          <div className="flex justify-center ">
-            <img
-              src="https://i.imgur.com/fyfri1v.png"
-              alt="Imagem Página Home"
-              className="w-2/3"
-            />
-          </div>
+          <p className="text-xl">
+            Expresse aqui seus pensamentos e opiniões
+          </p>
+
+          <button className="border-2 border-white rounded px-6 py-3 font-bold w-fit hover:bg-white hover:text-indigo-900 transition">
+            Nova Postagem
+          </button>
         </div>
+
+        {/* COLUNA DIREITA – IMAGEM */}
+        <div className="flex justify-center">
+          <img
+            src={home}
+            alt="Imagem Home"
+            className="w-full max-w-lg"
+          />
+        </div>
+
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
